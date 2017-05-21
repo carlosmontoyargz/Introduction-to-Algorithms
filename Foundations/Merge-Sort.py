02d0aece1f41b6966cb3d4a838b3c1761a9eecb8
Carlos Montoya <carlos.montoya.rdgz@gmail.com>
2017-05-21 16:32:41 -0500

    Agregada función llamadora

--- Foundations/Merge-Sort.py
+++ Foundations/Merge-Sort.py
@@ -6,7 +6,7 @@ import random
 
 def Merge(A, p, q, r):
     """Combina dos subsecuencias ordenadas dentro de un arreglo."""
-    n1 = q - p
+    n1 = q - p + 1
     n2 = r - q
 
     L = []
@@ -14,13 +14,13 @@ def Merge(A, p, q, r):
     for i in range(n1):
         L.append(A[p + i])
     for j in range(n2):
-        R.append(A[q + j])
+        R.append(A[q + j + 1])
     L.append(9999999)
     R.append(9999999)
 
     i = 0
     j = 0
-    for k in range(p, r):
+    for k in range(p, r + 1):
         if L[i] <= R[j]:
             A[k] = L[i]
             i += 1
@@ -29,17 +29,21 @@ def Merge(A, p, q, r):
             j += 1
 
 
-def MergeSort(A, p, r):
-    """Ordena la subsecuencia A[p..r] por medio del algoritmo Merge-Sort."""
-    if p < (r - 1):
+def MergeSortR(A, p, r):
+    """Ordena la sublista A[p..r] por medio del algoritmo Merge-Sort."""
+    if p < r:
         q = (p + r) / 2
-        MergeSort(A, p, q)
-        MergeSort(A, q, r)
+        MergeSortR(A, p, q)
+        MergeSortR(A, q + 1, r)
         Merge(A, p, q, r)
+
+
+def MergeSort(A):
+    """Ordena la lista A por medio del algoritmo MergeSort."""
+    MergeSortR(A, 0, len(A) - 1)
 
 
 A = random.sample(range(20), 20)
 print(A)
-
-MergeSort(A, 0, len(A))
+MergeSort(A)
 print(A)
